595be0e54f5a368e1a4e9e124f967d6db0b323c2
jtn-san <santosjonnathan@aol.com>
2025-03-14 09:26:25 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+    $(".grid").isotope({ itemSelector: ".item",});
+    
+    
+$(".filterGroup").on("click", "li", function(){
+    var filterValue = $(this).attr("data-filter");
+    $(".grid").isotope({ filter : filterValue});
+    $(".filterGroup li").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+
+
+window.addEventListener("load", ()=> {
+let video = window.document.querySelector("#video")
+video.play()
+video.loop = true
+
+});
+
+// temas
+const btnClaro = document.getElementById("lightMode");
+const btnEscuro = document.getElementById("darkMode");
+const body = document.body;
+body.classList.add("lightMode")
+btnClaro.style.display = "inline";
+btnEscuro.style.display = "none";
+
+btnClaro.addEventListener("click", ()=> {
+    body.classList.remove("lightMode");
+    body.classList.add("darkMode");
+    btnClaro.style.display = "none";
+    btnEscuro.style.display = "inline";
+    
+})
+
+btnEscuro.addEventListener("click", ()=> {
+    body.classList.remove("darkMode");
+    body.classList.add("lightMode");
+    btnClaro.style.display = "inline";
+    btnEscuro.style.display = "none";
+})
+
+
+// Estrutura active
+// essa estrutura retira e adiciona a classe active do html
+document.querySelectorAll('.navLink li').forEach((item) => {
+    item.addEventListener('click', function(){
+        // remove a classe active de todos os itens
+
+        document.querySelectorAll('.navLink li').forEach((li) =>{
+            li.classList.remove('active');
+        
+        });
+
+        // Adiciona a classe active ao item clicado
+        this.classList.add('active');
+
+    });
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+    const navBar = document.querySelector(".navBar");
+    const spans = document.querySelectorAll(".navLink li a span");
+
+    function toggleSpanVisibility(){
+        if(navBar.offsetWidth > 4 *16){ //convertendo 4rem para pixel
+            spans.forEach((span) => {
+                span.style.opacity = "0";
+                span.style.visibility = "visible";
+            }); 
+        }else{
+            spans.forEach((span)=>{
+                span.style.opacity = "1";
+                span.style.visibility= "hidden";
+
+            });
+        };
+    };
+    // esconde os spans inicialmente
+    toggleSpanVisibility();
+
+    navBar.addEventListener("mouseenter",()=>{
+        spans.forEach((span) => {
+            span.style.opacity = "1";
+            span.style.visibility = "visible";
+    })
+});
+        navBar.addEventListener("mouseleave",()=>{
+            spans.forEach((span) => {
+                span.style.opacity = "0";
+                span.style.visibility = "hidden";
+            })
+        })     
+});
+
+// filter
+// isotopo jQuery
+
+
+AOS.init({
+    duration: 1000,
+  });
+
+ // 13-03 
