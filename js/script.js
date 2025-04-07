3f3d0ca274b025b1f0ae424cf24027eff299a2e5
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2025-04-07 13:23:52 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+$(document).ready(function(){
+    $(".grid").isotope({ itemSelector: ".item",});
+    
+    
+$(".filterGroup").on("click", "li", function(){
+    var filterValue = $(this).attr("data-filter");
+    $(".grid").isotope({ filter : filterValue});
+    $(".filterGroup li").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
 window.addEventListener("load", ()=> {
 let video = window.document.querySelector("#video")
 video.play()
 video.loop = true
-});
 
+});
 
 // temas
 const btnClaro = document.getElementById("lightMode");
@@ -81,22 +93,12 @@ document.addEventListener("DOMContentLoaded",function(){
         })     
 });
 
+
 // filter
 // isotopo jQuery
-$(document).ready(function(){
-    $(".grid").isotope({ itemSelector: ".item",});
-    
-    
-$(".filterGroup").on("click", "li", function(){
-    var filterValue = $(this).attr("data-filter");
-    $(".grid").isotope({ filter : filterValue});
-    $(".filterGroup li").removeClass("active");
-    $(this).addClass("active");
-  });
-});
 
-AOS.init({
-    duration: 1000,
-  });
 
- // 13-03 
+
+// AOS.init({
+//     duration: 1000,
+//   });
