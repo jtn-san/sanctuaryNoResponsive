479e306d1f0f6c8d4b5b2432d8f35f4373ea32b1
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2025-04-07 11:54:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-    $(".grid").isotope({ itemSelector: ".item",});
-    
-    
-$(".filterGroup").on("click", "li", function(){
-    var filterValue = $(this).attr("data-filter");
-    $(".grid").isotope({ filter : filterValue});
-    $(".filterGroup li").removeClass("active");
-    $(this).addClass("active");
-  });
-});
-
 window.addEventListener("load", ()=> {
 let video = window.document.querySelector("#video")
 video.play()
@@ -95,7 +83,17 @@ document.addEventListener("DOMContentLoaded",function(){
 
 // filter
 // isotopo jQuery
-
+$(document).ready(function(){
+    $(".grid").isotope({ itemSelector: ".item",});
+    
+    
+$(".filterGroup").on("click", "li", function(){
+    var filterValue = $(this).attr("data-filter");
+    $(".grid").isotope({ filter : filterValue});
+    $(".filterGroup li").removeClass("active");
+    $(this).addClass("active");
+  });
+});
 
 AOS.init({
     duration: 1000,
